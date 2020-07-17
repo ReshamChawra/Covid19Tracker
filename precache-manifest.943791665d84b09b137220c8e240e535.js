d0bc758e229d6a8cfe57e3667fd36cfce5c041f8
Resham Chawra <reshamchawra18@gmail.com>
2020-07-17 17:47:59 +0530

    Updates

---
+++ precache-manifest.943791665d84b09b137220c8e240e535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40310570664aea5a3d4358a33431d9be",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "0bc031e3ff1d54727d1d",
+    "url": "/Covid19Tracker/static/css/main.a398118d.chunk.css"
+  },
+  {
+    "revision": "f8b8783e534f2e313373",
+    "url": "/Covid19Tracker/static/js/2.266a5264.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19Tracker/static/js/2.266a5264.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc031e3ff1d54727d1d",
+    "url": "/Covid19Tracker/static/js/main.75dc4a3e.chunk.js"
+  },
+  {
+    "revision": "9fa1dc6f1e487a84ceb5",
+    "url": "/Covid19Tracker/static/js/runtime-main.babf432b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19Tracker/static/media/image.d7265326.png"
+  }
+]);
